e7f393e6226515cd4c24c169950c61a377652405
alaminse <contactalamin.se@gmail.com>
2023-01-04 06:43:40 +0600

    Start

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+
+class RegisterController extends BaseController
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:users',
+            'phone' => 'required|min:11|numeric',
+            'password' => [
+                'required',
+                'string',
+                'min:6',             // must be at least 6 characters in length
+                'regex:/[a-z]/',      // must contain at least one lowercase letter
+                'regex:/[A-Z]/',      // must contain at least one uppercase letter
+                'regex:/[0-9]/',      // must contain at least one digit
+                'regex:/[@$!%*#?&]/', // must contain a special character
+            ],
+            'c_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $user = new User;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $success['remember_token'] =  $user->createToken($user->email)->accessToken;
+            User::where('email', $user->email)->update(['remember_token'=> $success['remember_token']]);
+            $success['email'] =  $user->email;
+
+            return $this->sendResponse($success, 'User register successfully.');
+        }
+        else{
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        }
+
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $success['remember_token'] =  $user->createToken($user->email)->accessToken;
+            User::where('email', $user->email)->update(['remember_token'=> $success['remember_token']]);
+            $success['email'] =  $user->email;
+
+            return $this->sendResponse($success, 'User login successfully.');
+        }
+        else{
+            return $this->sendError(' .', ['error'=>'Unauthorised']);
+        }
+    }
+}
